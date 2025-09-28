fe97f73994cbe3862029a33cfb356f5064c91367
Ahmed-code-star <vcfcdef780@gmail.com>
2025-09-28 13:55:31 +0300

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,21 @@
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAY4KAjhd4ULV85XorgUXDKtW748Jo_Ju8",
+  authDomain: "hi-tech-caeab.firebaseapp.com",
+  projectId: "hi-tech-caeab",
+  storageBucket: "hi-tech-caeab.appspot.com",
+  messagingSenderId: "493997466961",
+  appId: "1:493997466961:web:4aa242a63f03ff71441f70",
+  measurementId: "G-M68F8YSY52"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
